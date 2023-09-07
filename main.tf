8abce2f59009dd1eb11f9672c8863464a7b54196
Nicolas Harlem Eide <nicolas@harlemeide.net>
2023-09-07 12:50:03 +0200

    Add implementation

---
+++ main.tf
@@ -0,0 +1,68 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name = "${var.name}-lambda"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.name
+
+  package_type = var.artifact_type
+
+  s3_bucket         = var.artifact_type == "s3" ? var.artifact.store : null
+  s3_key            = var.artifact_type == "s3" ? var.artifact.path : null
+  s3_object_version = var.artifact_type == "s3" ? var.artifact.version : null
+  image_uri         = var.artifact_type == "ecr" ? "${var.artifact.store}/${var.artifact.path}@${var.artifact.version}" : null
+
+  timeout = 120
+
+  runtime = var.artifact_type == "s3" ? var.runtime : null
+  handler = var.handler
+
+  memory_size = var.memory
+
+  role = aws_iam_role.this.arn
+
+  environment {
+    variables = var.environment_variables
+  }
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${aws_lambda_function.this.function_name}"
+  retention_in_days = 30
+}
+
+data "aws_iam_policy_document" "allow_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      aws_cloudwatch_log_group.this.arn,
+      "${aws_cloudwatch_log_group.this.arn}:*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "allow_logging" {
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.assume_role_policy.json
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "name" {
+  description = "The name of the lambda function"
+
+  type = string
+}
+
+variable "artifact_type" {
+  description = "The type of artifact to deploy"
+
+  type = string
+
+  validation {
+    condition     = contains(["s3", "ecr"], var.artifact_type)
+    error_message = "Artifact type must be one of `s3` or `ecr`."
+  }
+}
+
+variable "artifact" {
+  type = object({
+    store   = string
+    path    = string
+    version = string
+  })
+}
+
+variable "runtime" {
+  description = "The runtime to use for the lambda function"
+
+  type = string
+}
+
+variable "handler" {
+  description = "The handler to use for the lambda function"
+
+  type = string
+}
+
+variable "environment_variables" {
+  description = "Environment variables to set for the lambda function"
+
+  type = map(string)
+}
+
+variable "memory" {
+  description = "The amount of memory to allocate to the lambda function"
+
+  type    = number
+  default = 128
+}
