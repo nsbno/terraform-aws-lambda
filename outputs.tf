33b1c47a29fc29b07a078923886cfd1191b743af
Nicolas Harlem Eide <nicolas@harlemeide.net>
2023-09-29 15:46:18 +0200

    Add `invoke_arn` and `function_name` to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "lambda_arn" {
   value = aws_lambda_function.this.arn
 }
 
+output "invoke_arn" {
+  value = aws_lambda_function.this.invoke_arn
+}
+
+output "function_name" {
+  value = aws_lambda_function.this.function_name
+}
+
 output "role_arn" {
   value = aws_iam_role.this.arn
 }
