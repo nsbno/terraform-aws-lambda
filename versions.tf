3e4584802c1a56357ac61bbf8ef2677328565cbf
Wictor Zhao <wictor_x@hotmail.com>
2024-09-03 12:08:11 +0200

    Add datadog provider to be able to destroy resources

--- versions.tf
+++ versions.tf
@@ -6,5 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.26.0"
     }
+    datadog = {
+      source  = "DataDog/datadog"
+      version = ">= 3.40.0"
+    }
   }
+}
+
+provider "datadog" {
+  api_key = data.aws_secretsmanager_secret_version.datadog_api_key.secret_string
+  app_key = data.aws_secretsmanager_secret_version.datadog_app_key.secret_string
+
+  api_url = "https://api.datadoghq.eu/"
 }
