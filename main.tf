6e76fee345a339cd946a9f2e66adb21cc7f205af
Wictor Zhao <wictor_x@hotmail.com>
2025-07-09 10:47:07 +0200

    Revert "Add edgelambda as a principal identifier"
    This reverts commit 2540ba1079e20e094208ebb08bd439038190a7e3.

--- main.tf
+++ main.tf
@@ -9,10 +9,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
     principals {
       type        = "Service"
-      identifiers = [
-		"edgelambda.amazonaws.com",
-		"lambda.amazonaws.com"
-	  ]
+      identifiers = ["lambda.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
